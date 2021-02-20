d298cc75ca71ca26f996f6f37082cf5c8e9ea00d
Dennis Cosgrove <cosgroved@wustl.edu>
2021-02-20 01:52:00 -0600

    spring 2021 updates

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -4,69 +4,82 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Methods {
 
-    /**
-     * Compute the (Euclidean) distance between two points.
-     *
-     * @param x1 x-coordinate of a point
-     * @param y1 y-coordinate of a point
-     * @param x2 x-coordinate of another point
-     * @param y2 y-coordinate of another point
-     * @return the Euclidean distance between (x1,y1) and (x2,y2)
-     */
-    public static double distanceBetween(double x1, double y1, double x2, double y2) {
-        double distance = 0;
-        // FIXME: Hint use Math methods (Math.pow, Math,sqrt, etc.) to compute the distance
-        return distance;
-    }
+	/**
+	 * Compute the (Euclidean) distance between two points.
+	 *
+	 * @param x1 x-coordinate of a point
+	 * @param y1 y-coordinate of a point
+	 * @param x2 x-coordinate of another point
+	 * @param y2 y-coordinate of another point
+	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
+	 */
+	public static double distanceBetween(double x1, double y1, double x2, double y2) {
+		double distance = 0;
+		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
+		
+		return distance;
+	}
 
-    /**
-     * Compute the sum of elements in an array
-     * @param values an array of integers
-     * @return the sum of the elements in values
-     */
-    public static int arraySum(int[] values) {
-        int sum = 0;
-        // FIXME: Compute the sum of the values in an array
-        return sum;
-    }
+	/**
+	 * Compute the sum of elements in an array
+	 * 
+	 * @param values an array of integers
+	 * @return the sum of the elements in values
+	 */
+	public static int arraySum(int[] values) {
+		int sum = 0;
+		// FIXME: Compute the sum of the values in an array
+		
+		return sum;
+	}
 
-    /**
-     * Return an array of a given size filled with the provided value
-     * @param size the size of the returned array
-     * @param value the value to fill the array with
-     * @return and array of size that's filled with value
-     */
-    public static int[] filledArray(int size, int value) {
-        int[] array = null;  // FIXME: Create an array of the appropriate size
-        // TODO: Finish this method
-        return array;
-    }
+	/**
+	 * Return an array of a given size filled with the provided value
+	 * 
+	 * @param length the length of the returned array
+	 * @param value  the value to fill the array with
+	 * @return and array of size that's filled with value
+	 */
+	public static int[] filledArray(int length, int value) {
+		int[] values = null; // FIXME: Create an array of the appropriate size
+		// TODO: Finish this method
 
-    // TODO: Create a `arrayMean()` method and JavaDoc for it.
+		
 
+		return values;
+	}
 
+	// TODO: Create an arrayMean method which accepts an int array of values parameter.
+	// TODO: Create a JavaDoc comment for the arrayMean method.
 
+	
 
+	/**
+	 * Draw a bull's eye at the given location with the given radius.
+	 *
+	 * @param x      the x coordinate of the center of the bull's eye
+	 * @param y      the y coordinate of the center of the bull's eye
+	 * @param radius the radius of the bull's eye
+	 */
+	public static void drawBullsEye(double x, double y, double radius) {
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledCircle(x, y, radius);
 
+		// TODO: Draw the remaining rings of the bull's eye
+		// Blue ring with 3.0/4.0 the radius
+		// suggested rgb values: 0, 109, 219
 
-    /**
-     * Draw a bull's eye at the given location with the given radius.
-     *
-     * @param x the x coordinate of the center of the bull's eye
-     * @param y the y coordinate of the center of the bull's eye
-     * @param radius the radius of the bull's eye
-     */
-    public static void bullsEye(double x, double y, double radius) {
-        StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(x,y, radius);
+		
 
-        // TODO: Draw the remaining rings of the bull's eye
-        // Blue ring with 3/4 the radius
+		// Red ring with 1.0/2.0 the radius
+		// suggested rgb values: 146, 0, 0
 
-        // Red ring with 1/2 the radius
+		
 
-        // Yellow ring with 1/4 the radius
+		// Yellow ring with 1.0/4.0 the radius
+		// suggested rgb values: 255, 255, 109
 
-    }
+		
+	}
 
 }
---
+++ src/studio5/MethodsTestSuite.java
@@ -0,0 +1,20 @@
+package studio5;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import studio5.tests.ArrayMeanTestSuite;
+import studio5.tests.ArraySumTestSuite;
+import studio5.tests.DistanceBetweenTest;
+import studio5.tests.FilledArraySumAndMeanComprehensiveTest;
+import studio5.tests.FilledArrayTestSuite;
+
+/**
+ * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ DistanceBetweenTest.class, ArraySumTestSuite.class, FilledArrayTestSuite.class,
+		ArrayMeanTestSuite.class, FilledArraySumAndMeanComprehensiveTest.class })
+public class MethodsTestSuite {
+
+}
